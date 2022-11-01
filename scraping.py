ed823d10992eccc965c6175da839bee29def1e21
snehannnnnn <95863218+sanbec234@users.noreply.github.com>
2022-11-02 01:13:04 +0530

    Update scraping.py
    enter the correct goto page links and inner_html tags to access the needed content of the resp html tag from the page.

--- scraping.py
+++ scraping.py
@@ -16,11 +16,11 @@ password = input("enter password: ")
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False, slow_mo=1000)
     page = browser.new_page()
-    page.goto("https://ecampus.psgtech.ac.in/studzone2/")
+    page.goto("")
     page.fill('input#txtusercheck', rollno)
     page.fill('input#txtpwdcheck', password)
     page.click('input[name = abcd3]')
-    page.goto("https://ecampus.psgtech.ac.in/studzone2/AttWfPercView.aspx")
+    page.goto("")
 
     name = page.inner_html('#TbStudInfo > tbody > tr:nth-child(1) > td.GenName')
     soup = BeautifulSoup(name, 'html.parser')
